e25a3f4cc4ae4ad642875cd13e6e5eccb667d97e
Ivan Pavlov <i.pavlov@moda.live>
2025-08-14 13:18:57 -0500

    Some changes.

--- src/main/java/com/example/demo_containers/DemoContainersApplication.java
+++ src/main/java/com/example/demo_containers/DemoContainersApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.stream.Stream;
 
@@ -19,6 +21,14 @@ public class DemoContainersApplication {
         SpringApplication.run(DemoContainersApplication.class, args);
     }
 
+//    https://spring.io/quickstart
+//    @GetMapping("/hello")
+//    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+//        return String.format("Hello %s!", name);
+//    }
+
+
+//    https://learn.microsoft.com/en-us/azure/developer/java/spring-framework/configure-spring-data-jdbc-with-azure-postgresql?tabs=passwordless%2Cservice-connector&pivots=postgresql-passwordless-flexible-server
     @Bean
     ApplicationListener<ApplicationReadyEvent> basicsApplicationListener(TodoRepository repository) {
         return event -> repository
